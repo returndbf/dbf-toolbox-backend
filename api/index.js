9000a13fc76ea9603604d16a9931100ae41a7e45
returndbf <1668959325@qq.com>
2024-12-21 21:57:02 +0800

    refactor: 合并天气请求

--- api/index.js
+++ api/index.js
@@ -5,17 +5,24 @@ app.get('/reqTranslate', async (req, res) => {
     const r = await request('https://api.niutrans.com/NiuTransServer/translation', 'GET', {...req.query,apikey:process.env.TRANSLATE_KEY});
     res.send(r);
 });
-app.get('/reqLocation', async (req, res) => {
-    const r = await request(`https://geoapi.qweather.com/v2/city/lookup`, 'GET', {
-        key: process.env.WEATHER_KEY,
-        ...req.query,
-    })
-    res.send(r);
-});
 app.get('/reqWeather', async (req, res) => {
-    const r = await request(`https://devapi.qweather.com/v7/weather/3d`, 'GET', {
+    let weatherData = null
+    const {location} = await request(`https://geoapi.qweather.com/v2/city/lookup`, 'GET', {
         key: process.env.WEATHER_KEY,
         ...req.query,
     })
-    res.send(r);
+    if (location) {
+        weatherData = await request(`https://devapi.qweather.com/v7/weather/3d`, 'GET', {
+            key: process.env.WEATHER_KEY,
+            location:location?.[0]?.id
+        })
+    }
+    res.send(weatherData);
 });
+// app.get('/reqWeather', async (req, res) => {
+//     const r = await request(`https://devapi.qweather.com/v7/weather/3d`, 'GET', {
+//         key: process.env.WEATHER_KEY,
+//         ...req.query,
+//     })
+//     res.send(r);
+// });
