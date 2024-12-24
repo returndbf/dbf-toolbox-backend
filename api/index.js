a6eef81f021d1bb868967a6543cf9c0bbf4f0ac4
returndbf <1668959325@qq.com>
2024-12-24 21:07:27 +0800

    feat: 删除无用注释

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import app from "../index.js"
 import request from "../fetch.js"
 app.get('/reqTranslate', async (req, res) => {
-    // const {from = 'zh', to = 'en', src_text = '1'} = req.query;
     const r = await request('https://api.niutrans.com/NiuTransServer/translation', 'GET', {...req.query,apikey:process.env.TRANSLATE_KEY});
     res.send(r);
 });
@@ -20,10 +19,3 @@ app.get('/reqWeather', async (req, res) => {
     }
     res.send({weatherData,position:locationData});
 });
-// app.get('/reqWeather', async (req, res) => {
-//     const r = await request(`https://devapi.qweather.com/v7/weather/3d`, 'GET', {
-//         key: process.env.WEATHER_KEY,
-//         ...req.query,
-//     })
-//     res.send(r);
-// });
